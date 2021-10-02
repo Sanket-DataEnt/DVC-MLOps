ccd474cdffd351e8987565171435354f6db77afb
Sanket Maheshwari <sanketmaheshwari96@gmail.com>
2021-10-02 11:13:12 -0400

    stage 1 added

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -3,8 +3,13 @@ import os
 
 
 
-def read_yml(path_to_yml: str) -> dict:
+def read_yaml(path_to_yml: str) -> dict:
     with open(path_to_yml) as yaml_file:
         content = yaml.safe_load(yaml_file)
 
     return content
+
+def create_directory(dirs: list):
+    for dir_path in dirs:
+        os.makedirs(dir_path, exist_ok=True)
+        print(f"directory is created at {dir_path}")
